676377b02d7f5c9db580f1327c52d252b43a5539
ibrahimaliyevv2 <7cauc3b@code.edu.az>
2022-06-03 13:44:45 +0400

    hometasks added

---
+++ EternaTemplate/EternaTemplate/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EternaTemplate.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Blog()
+        {
+            return View();
+        }
+    }
+}
---
+++ EternaTemplate/EternaTemplate/Controllers/PortfolioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EternaTemplate.Controllers
+{
+    public class PortFolioController : Controller
+    {
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+    }
+}
